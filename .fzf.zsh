fd6c17714e0110d2a2c8b0acd3702a35f6b9d9e5
andres06-hub <andressierrarojas7@gmail.com>
2023-02-25 04:01:24 -0500

    CREATED: plugin of zsh

---
+++ .fzf.zsh
@@ -0,0 +1,20 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */home/as_dev/.fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/home/as_dev/.fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/home/as_dev/.fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/home/as_dev/.fzf/shell/key-bindings.zsh"
+# Alias
+alias fv="fzf --print0 | xargs -0 -o nvim"
+alias ft="fzf-tmux"
+
+# Style
+export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --border'
+
