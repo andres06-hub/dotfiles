c786518b2048d25ae9585b43a39aad9c7801a5be
andres06-hub <andressierrarojas7@gmail.com>
2023-02-26 04:17:15 -0500

    MODIFIED: fzf with Bat and Nvim

--- .fzf.zsh
+++ .fzf.zsh
@@ -11,10 +11,22 @@ fi
 # Key bindings
 # ------------
 source "/home/as_dev/.fzf/shell/key-bindings.zsh"
-# Alias
-alias fv="fzf --print0 | xargs -0 -o nvim"
-alias ft="fzf-tmux"
+
+# Open whit vim
+N="0"
+XV="xargs -$N -o nvim"
+
+#Config bat : styles
+alias sbat="'bat -n --color=always {}'"
+
+# Aliases
+alias fv="fzf --preview $sbat --print$N | $XV"
+alias ft="fzf-tmux | xclip -sel c"
 
 # Style
-export FZF_DEFAULT_OPTS="--height=40% --layout=reverse --info=inline --border --margin=1 --padding=1"
+export FZF_DEFAULT_OPTS="--height=90% --layout=default --info=inline --border --margin=1 --padding=1"
+
+# - Popup window (center of the current window)
+#let g:fzf_layout={ 'window': { 'width': 0.9, 'height': 0.6, 'relative': v:true } }
+
 
