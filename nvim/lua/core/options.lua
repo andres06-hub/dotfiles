77aa262c11d5543c5d7e23623eaee58dce8cd00c
andres06-hub <andressierrarojas7@gmail.com>
2023-02-27 01:46:36 -0500

    MODIFIED: options

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -1,32 +1,41 @@
+local options = {
+  -- Line numbers
+  relativenumber = true,
+
+  -- Tabs and identation 
+  tabstop = 2,
+  shiftwidth = 2,
+  expandtab = true,
+  autoindent = true,
+  ai = true, -- Auto Indent
+  si = true, -- Smart Indent
+
+  -- Line Wrapping
+  wrap = false, -- No wrap lines
+
+  -- Search settings
+  ignorecase = true,
+
+  -- Windows : Set the splits to open at the right side and below 
+  splitright = true,
+  splitbelow = true,
+
+  -- Appareance
+  termguicolors = true,
+
+  -- To avoid performance issues, I never fold anything  
+  -- foldmethod = manual
+
+}
+
 local opt = vim.opt
-
--- Line numbers
-opt.relativenumber = true
-
--- Tabs and identation 
-opt.tabstop = 2
-opt.shiftwidth = 2
-opt.expandtab = true
-opt.autoindent = true
-opt.ai = true -- Auto Indent
-opt.si = true -- Smart Indent
-
--- Line Wrapping
-opt.wrap = false -- No wrap lines
-
--- Search settings
-opt.ignorecase = true
-
--- Windows : Set the splits to open at the right side and below 
-opt.splitright = true
-opt.splitbelow = true
 
 -- Search
 opt.path:append { '**' } -- Finding files -Search down into subfolders
 opt.wildignore:append { '*/node_modules*' }
 
--- Appareance
-opt.termguicolors = true
 
--- To avoid performance issues, I never fold anything  
--- opt.foldmethod = manual
+
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
