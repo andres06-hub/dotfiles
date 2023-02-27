0b44bd1d081410384b43cf6e328896aff481c796
andres06-hub <andressierrarojas7@gmail.com>
2023-02-27 01:19:30 -0500

    ADD: packer

--- nvim/lua/plugins-setup.lua
+++ nvim/lua/plugins-setup.lua
@@ -1,13 +1,38 @@
--- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+-- validate if exist packer
+local ensure_packer = function()
+    local fn = vim.fn
+    local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
+end
+
+-- autocommand that reloads neovim and installs/updates/removes plugins
+-- when file is saved
+vim.cmd([[
+augroup packer_user_config
+autocmd!
+autocmd BufWritePost plugins-setup.lua source <afile> | PackerSync
+augroup end
+]])
+
+-- build the packer, validate if the packer is installed
+local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
     -- packer can manager itself
     use('wbthomason/packer.nvim')
     -- colorscheme
     use 'navarasu/onedark.nvim' -- One Dark Theme
+
     use {
         'nvim-telescope/telescope.nvim', -- Search files
         requires = { {'nvim-lua/plenary.nvim'} } -- Easy functions writting
    }
+   if packer_bootstrap then
+	require('packer').sync()
+    end
 end)
